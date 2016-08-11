3ab414ba9a69f3f3915e18ca8cd922474476e0e8
Jacek Mikrut <jacekmikrut@gmail.com>
2016-08-11 17:51:54 +0200

    Introduced "data-prompt-trigger-type" attribute.

--- app/js/prompt-trigger-button-view.js
+++ app/js/prompt-trigger-button-view.js
@@ -1,11 +1,13 @@
 const PromptTriggerData = require('./prompt-trigger-data.js');
-const PromptTriggerType = require('./prompt-trigger-type.js');
 
 class PromptTriggerButtonView {
 
   constructor({ buttonElement, onClick }) {
     this.buttonElement = buttonElement;
-    this.promptTriggerData = new PromptTriggerData({ type: PromptTriggerType.INTERVAL, value: Number(this.buttonElement.getAttribute('data-prompt-trigger-value')) });
+    this.promptTriggerData = new PromptTriggerData({
+      type: this.buttonElement.getAttribute('data-prompt-trigger-type'),
+      value: Number(this.buttonElement.getAttribute('data-prompt-trigger-value'))
+    });
     this.buttonElement.addEventListener('click', () => { this.buttonElement.blur(); onClick(this); });
   }
 
